1ef6e22831f940ccbfb8cdc8bc66cdbaf62b8cfa
username <email>
2022-01-03 13:31:24 +0000

    gh-pages

---
+++ precache-manifest.b86f1490d8d258a57d4bfffdc0dc80a9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f32176eed117fb80127753f6334e28e",
+    "url": "/nft-statements/content/ea.json"
+  },
+  {
+    "revision": "d66c3702571107bad5883d9f1a3dccb9",
+    "url": "/nft-statements/content/epic-games-store.json"
+  },
+  {
+    "revision": "b6403a435b634a7720e577fe29cf271b",
+    "url": "/nft-statements/content/fortnite.json"
+  },
+  {
+    "revision": "e06372bb9b5624f3cb85dd5b155737e0",
+    "url": "/nft-statements/content/habbo.json"
+  },
+  {
+    "revision": "14fd2de91fb0dbaba1119a506f02439c",
+    "url": "/nft-statements/content/index.json"
+  },
+  {
+    "revision": "a5f42d6a8951e101c564d632d967247f",
+    "url": "/nft-statements/content/kenney.json"
+  },
+  {
+    "revision": "5b8232aa5ea3660e26d5a9fc3f592a06",
+    "url": "/nft-statements/content/new-blood.json"
+  },
+  {
+    "revision": "70edfda5d1c7d0c0f3bbf798cf825b05",
+    "url": "/nft-statements/content/square-enix.json"
+  },
+  {
+    "revision": "2ecc6a04e3fdeecc326c287c0aff6812",
+    "url": "/nft-statements/content/ubisoft.json"
+  },
+  {
+    "revision": "9e62a32ac1e3c01e1c110cf86dcb4645",
+    "url": "/nft-statements/content/valve.json"
+  },
+  {
+    "revision": "69e93184307b2c2d7530",
+    "url": "/nft-statements/css/app.9ffb9222.css"
+  },
+  {
+    "revision": "1a8f079a44879893e6c9",
+    "url": "/nft-statements/css/chunk-vendors.3ca027ab.css"
+  },
+  {
+    "revision": "9cacdc876e2049988fcab540c21738d5",
+    "url": "/nft-statements/fonts/materialdesignicons-webfont.9cacdc87.eot"
+  },
+  {
+    "revision": "9d243c168a4f1c2cb3cec74884344de7",
+    "url": "/nft-statements/fonts/materialdesignicons-webfont.9d243c16.woff2"
+  },
+  {
+    "revision": "a0711490bcd581b647329230b3e915cf",
+    "url": "/nft-statements/fonts/materialdesignicons-webfont.a0711490.woff"
+  },
+  {
+    "revision": "b62641afc9ab487008e996a5c5865e56",
+    "url": "/nft-statements/fonts/materialdesignicons-webfont.b62641af.ttf"
+  },
+  {
+    "revision": "074547ee8ce680dee04d2bdf38a8cc1d",
+    "url": "/nft-statements/index.html"
+  },
+  {
+    "revision": "69e93184307b2c2d7530",
+    "url": "/nft-statements/js/app.8ee12052.js"
+  },
+  {
+    "revision": "1a8f079a44879893e6c9",
+    "url": "/nft-statements/js/chunk-vendors.56eac163.js"
+  },
+  {
+    "revision": "d949a879a63ad1024d6f1fcd632b01c0",
+    "url": "/nft-statements/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nft-statements/robots.txt"
+  }
+]);
